a82a0d9337159981c280c8906e02414e0a1cae43
Yakiyo <yakiyosama5855@gmail.com>
2023-04-30 14:34:00 +0000

    chore: impromptu commit

--- src/commands/install.rs
+++ src/commands/install.rs
@@ -1,26 +1,35 @@
 use super::command::Command;
 use clap::Args;
-pub const CHANNELS: [&str; 3] = ["stable", "beta", "dev"];
+
+/// Possible channels for the Dart SDK
+#[derive(Debug)]
+enum CHANNELS {
+    Stable,
+    Beta,
+    Dev
+}
+// pub const _FLAVOURS: [&str; 2] = ["release", "raw"];
 
 #[derive(Args, Debug, Default)]
 pub struct Install {
     /// The version to install. Use `latest` to indicate the latest release
     pub version: String,
 
-    #[arg(value_parser = is_valid_channel)]
-    /// The channel to install (stable, beta, dev). Defaults to `stable`
-    pub channel: Option<String>,
+    /// Install latest version
+    #[clap(long, conflicts_with_all = &["version"])]
+    pub latest: bool,
 }
 
 impl Command for Install {
     fn run(self) -> Result<(), String> {
+        let channel = if self.version.ends_with("beta") {
+            CHANNELS::Beta
+        } else if self.version.ends_with("dev") {
+            CHANNELS::Dev
+        } else {
+            CHANNELS::Stable
+        };
+        println!("{:#?}", channel);
         Ok(())
     }
 }
-
-fn is_valid_channel(s: &str) -> Result<String, String> {
-    match CHANNELS.contains(&s) {
-        true => Ok(s.to_string()),
-        false => Err(format!("Invalid channel type. Must be one of {}", CHANNELS.join(", ")))
-    }
-} 
