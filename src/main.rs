605858db831fe6a586e8f18d2f77cd3604cd97d7
Yakiyo <yakiyosama5855@gmail.com>
2023-04-30 22:00:46 +0600

    refactor: desc before import

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
-mod cli;
-mod arch;
 /// DSM - Dart SDK Manager
 ///
 /// A version manager for the dart sdk, written in rust
 /// Copyright 2023 Yakiyo. All rights reserved. MIT license.
+
 mod commands;
+mod cli;
+mod arch;
 
 fn main() {
     let args = cli::parse();
