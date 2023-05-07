2c99a5dcbc621e035adf3dadefe942bef82fa48f
Yakiyo <yakiyosama5855@gmail.com>
2023-05-07 14:11:05 +0000

    feat: impromptu commit

--- src/commands/install.rs
+++ src/commands/install.rs
@@ -1,14 +1,28 @@
-use crate::cli::DsmConfig;
-
 use super::Command;
+use crate::cli::DsmConfig;
 use clap::Args;
+use std::fs;
 
 /// Possible channels for the Dart SDK
 #[derive(Debug)]
-enum CHANNELS {
+enum Channel {
     Stable,
     Beta,
     Dev,
+}
+
+impl std::str::FromStr for Channel {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        if s.ends_with("beta") {
+            return Ok(Channel::Beta);
+        } else if s.ends_with("dev") {
+            return Ok(Channel::Dev);
+        };
+
+        Ok(Channel::Stable)
+    }
 }
 
 #[derive(Args, Debug, Default)]
@@ -18,15 +32,16 @@ pub struct Install {
 }
 
 impl Command for Install {
-    fn run(self, config: DsmConfig) -> Result<(), String> {
-        let channel = if self.version.ends_with("beta") {
-            CHANNELS::Beta
-        } else if self.version.ends_with("dev") {
-            CHANNELS::Dev
-        } else {
-            CHANNELS::Stable
-        };
-        println!("{:#?}, {:#?}", channel, config.arch);
+    fn run(self, config: &DsmConfig) -> Result<(), String> {
+        let channel: Channel = self.version.parse().unwrap();
+        let mut home_dir = crate::cli::home_dir();
+        home_dir.push("/.dsm");
+        let dir = config.base_dir.as_ref().unwrap_or(&home_dir);
+        println!("{:#?}, {}, {:#?}", channel, config.arch, dir);
+        if !dir.exists() {
+            fs::create_dir_all(dir).unwrap();
+        }
+        println!("{} {} {}", dir.is_dir(), dir.is_file(), dir.exists());
         Ok(())
     }
 }
