c373c8eea5eceba8b184809af2ad211cb896a989
Yakiyo <yakiyosama5855@gmail.com>
2023-05-07 14:36:47 +0000

    feat: log funcs

---
+++ src/log.rs
@@ -0,0 +1,26 @@
+use yansi::Paint;
+
+/// Print error messae
+pub fn error(message: &str) {
+    eprintln!("{} {}", Paint::red("[ERROR]").bold(), message);
+}
+
+/// Print warning messae
+pub fn warn(message: &str) {
+    println!("{} {}", Paint::yellow("[WARN]").bold(), message);
+}
+
+/// Print info message
+pub fn info(message: &str) {
+    println!("{} {}", Paint::green("[INFO]").bold(), message);
+}
+
+/// Print debug message by passing the arg to the `info` function
+/// 
+/// This will only print them if the `DEBUG` env is set
+pub fn debug(message: &str) {
+    use std::env;
+    if env::var("DEBUG").is_ok(){
+        info(message);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,18 @@
-mod arch;
-mod cli;
-/// DSM - Dart SDK Manager
-///
-/// A version manager for the dart sdk, written in rust
-/// Copyright 2023 Yakiyo. All rights reserved. MIT license.
+//! DSM - Dart SDK Manager
+//!
+//! A version manager for the dart sdk, written in rust
+//! Copyright 2023 Yakiyo. All rights reserved. MIT license.
+//! 
 mod commands;
 mod platform;
+mod arch;
+mod cli;
+mod log;
 
 fn main() {
+    
+    yansi::Paint::enable_windows_ascii();
+
     let args = cli::parse();
     args.handle_sub();
 }
