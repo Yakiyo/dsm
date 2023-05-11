26ae4d9694c44c2d5de9124981ee44c8a67afc25
Yakiyo <yakiyo1206@gmail.com>
2023-05-11 15:42:09 +0000

    feat: impl default trait and use Version

--- src/commands/install.rs
+++ src/commands/install.rs
@@ -1,36 +1,19 @@
 use super::Command;
 use crate::cli::DsmConfig;
 use crate::log::debug;
-use crate::version::Channel;
+use crate::version::Version;
 use clap::Args;
 use std::fs;
 use std::path;
 
-
-impl std::str::FromStr for Channel {
-    type Err = ();
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        if s.ends_with("beta") {
-            return Ok(Channel::Beta);
-        } else if s.ends_with("dev") {
-            return Ok(Channel::Dev);
-        };
-
-        Ok(Channel::Stable)
-    }
-}
-
 #[derive(Args, Debug, Default)]
 pub struct Install {
-    /// The version to install. Use `latest` to indicate the latest release
-    pub version: String,
+    /// The version to install.
+    pub version: Version,
 }
 
 impl Command for Install {
     fn run(self, config: &DsmConfig) -> Result<(), String> {
-        let _channel: Channel = self.version.parse().unwrap();
-
         let home_dir: path::PathBuf = crate::cli::home_dir().as_path().join("/.dsm");
 
         let dir = config.base_dir.as_ref().unwrap_or(&home_dir);
