c24a9b1df52ff0f500efd54ec3cf5d16d00623cc
Yakiyo <yakiyo1206@gmail.com>
2023-05-27 15:19:56 +0000

    refactor: smoll changes

--- src/commands/install.rs
+++ src/commands/install.rs
@@ -1,34 +1,34 @@
-use std::io::Write;
-
-use super::Command;
-use crate::{arch::Arch, cli::DsmConfig, platform::platform_name, http::fetch_bytes, debug};
-use anyhow::Context;
-use clap::Args;
 use crate::version::Version;
+use crate::arch::Arch;
+use crate::cli::DsmConfig;
+use crate::debug;
+use crate::http::fetch_bytes;
+use crate::platform::platform_name;
+use anyhow::Context;
+use std::io::Write;
+use zip::read::ZipArchive;
 use spinners::{Spinner, Spinners};
 
-#[derive(Args, Debug, Default)]
+#[derive(clap::Args, Debug, Default)]
 pub struct Install {
     /// The version to install.
     pub version: Version,
 }
 
-impl Command for Install {
+impl super::Command for Install {
     fn run(self, config: DsmConfig) -> anyhow::Result<()> {
         let dir = &config.base_dir;
 
-        // match dir.ensure_dirs() {
-        //     Ok(_) => {}
-        //     Err(e) => return Err(Box::new(e)),
-        // }
         dir.ensure_dirs().context("Failed to setup dsm dirs")?;
-
         let mut sp = Spinner::new(
             Spinners::Line,
             format!("Downloading Dart SDK {}", self.version),
         );
 
-        install_dart_sdk(&self.version, &config, &mut sp)?;
+        if let Err(e) = install_dart_sdk(&self.version, &config) {
+            sp.stop();
+            return Err(e);
+        }
 
         sp.stop_and_persist("✔", "Downloaded Dart sdk".into());
 
@@ -40,13 +40,19 @@ impl Command for Install {
 fn install_dart_sdk(
     version: &Version,
     config: &DsmConfig,
-    _sp: &mut Spinner,
+    // _sp: &mut Spinner,
 ) -> anyhow::Result<()> {
     let archive = fetch_bytes(archive_url(version, &config.arch))?;
     debug!("Writing archive file to tempfile");
     let mut tmp = tempfile::tempfile().context("Failed to create temporary file")?;
-    tmp.write_all(&archive).context("Failed to write contents to temp file")?;
+    tmp.write_all(&archive)
+        .context("Failed to write contents to temp file")?;
 
+    ZipArchive::new(tmp)
+        .context("Failed to read ZipArchive")?
+        .extract(config.base_dir.find_version_dir(version).0)
+        .context("Failed to extract content from zip file")?;
+    
     Ok(())
 }
 
