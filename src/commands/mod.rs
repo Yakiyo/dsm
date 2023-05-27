d4682715d2753cf67404961f20e4c839a21aa4ec
Yakiyo <yakiyo1206@gmail.com>
2023-05-27 15:39:11 +0000

    feat: change error handling to anyhow

--- src/commands/mod.rs
+++ src/commands/mod.rs
@@ -1,12 +1,14 @@
 use crate::cli::DsmConfig;
+use yansi::Paint;
 use std::error::Error;
+
 pub mod install;
 
 pub trait Command: Sized {
     fn run(self, config: DsmConfig) -> anyhow::Result<()>;
 
     fn catch(err: Box<dyn Error>) {
-        eprintln!("Error: {:?}", err);
+        eprintln!("{} {}", Paint::red("[ERROR]"), err);
         std::process::exit(1);
     }
 
