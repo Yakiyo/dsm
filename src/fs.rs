2b0a94f2fd05ce41b154207f7cc77bc7293e6547
Yakiyo <yakiyo1206@gmail.com>
2023-05-27 16:59:17 +0000

    feat: symlink utils

---
+++ src/fs.rs
@@ -0,0 +1,32 @@
+/// This code is taken from Schniz/fnm. View: https://github.com/Schniz/fnm/blob/master/src/fs.rs
+/// All credits go to the contributors of that file
+
+use std::path::Path;
+
+#[cfg(unix)]
+pub fn symlink_dir<P: AsRef<Path>, U: AsRef<Path>>(from: P, to: U) -> std::io::Result<()> {
+    std::os::unix::fs::symlink(from, to)?;
+    Ok(())
+}
+
+#[cfg(windows)]
+pub fn symlink_dir<P: AsRef<Path>, U: AsRef<Path>>(from: P, to: U) -> std::io::Result<()> {
+    junction::create(from, to)?;
+    Ok(())
+}
+
+#[cfg(windows)]
+pub fn remove_symlink_dir<P: AsRef<Path>>(path: P) -> std::io::Result<()> {
+    std::fs::remove_dir(path)?;
+    Ok(())
+}
+
+#[cfg(unix)]
+pub fn remove_symlink_dir<P: AsRef<Path>>(path: P) -> std::io::Result<()> {
+    std::fs::remove_file(path)?;
+    Ok(())
+}
+
+pub fn shallow_read_symlink<P: AsRef<Path>>(path: P) -> std::io::Result<std::path::PathBuf> {
+    std::fs::read_link(path)
+}
