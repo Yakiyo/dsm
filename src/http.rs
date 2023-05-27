9ed38665f2b231fe380270e6f329d38f3fd6832e
Yakiyo <yakiyo1206@gmail.com>
2023-05-27 06:46:45 +0000

    feat: update http handlers

--- src/http.rs
+++ src/http.rs
@@ -1,27 +1,42 @@
 //! A module for handling http interactivity. A separate module so that
 //! when needed, we can easily switch libs
 
-use reqwest::blocking::get;
-use reqwest::blocking::Response;
+use anyhow::Context;
+use ureq::{get, Response};
 
-/// GET request
-pub fn _fetch<P: AsRef<str>>(url: P) -> anyhow::Result<Response> {
-    match get(url.as_ref()) {
-        Ok(e) => Ok(e),
-        Err(e) => Err(anyhow::anyhow!(e))
-    }
+/// Base fetch method
+pub fn fetch<P: AsRef<str>>(url: P) -> anyhow::Result<Response> {
+    let resp = get(url.as_ref())
+        .call()
+        .context("Failed to make http request to {url}")?;
+    Ok(resp)
 }
 
-pub fn _fetch2() {}
+/// Fetch and convert response body to bytes
+pub fn fetch_bytes<P: AsRef<str>>(url: P) -> anyhow::Result<Vec<u8>> {
+    let resp = fetch(url)?;
+    let mut bytes = Vec::new();
+    resp.into_reader()
+        .read_to_end(&mut bytes)
+        .context("Failed to read bytes from http response")?;
+    Ok(bytes)
+}
 
 #[cfg(test)]
 mod tests {
     use super::*;
     #[test]
     fn fetch_test() {
-        let body = _fetch("https://nekos.best/api/v2/neko");
-        let body = body.unwrap();
-        let resp = body.text().unwrap();
-        println!("{resp}");
+        let body = fetch("https://nekos.best/api/v2/neko").unwrap();
+        assert!(body.into_string().is_ok());
+    }
+
+    #[test]
+    fn fetch_bytes_test() {
+        use std::fs;
+        let bytes =
+            fetch_bytes("https://nekos.best/api/v2/neko/57797897-9d5c-4ad1-8bb8-a3ba8eedbdfa.png")
+                .unwrap();
+        fs::write("image.png", bytes).unwrap();
     }
 }
