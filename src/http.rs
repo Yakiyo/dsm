bd22f927152a46024b45733e9d382f1fab0c6622
Yakiyo <yakiyo1206@gmail.com>
2023-05-27 05:21:55 +0000

    refactor: func name and add test

--- src/http.rs
+++ src/http.rs
@@ -1,14 +1,27 @@
 //! A module for handling http interactivity. A separate module so that
 //! when needed, we can easily switch libs
 
-use reqwest::blocking::get as rget;
+use reqwest::blocking::get;
 use reqwest::blocking::Response;
-use std::error::Error;
 
 /// GET request
-pub fn _get<P: AsRef<str>>(url: P) -> Result<Response, Box<dyn Error>> {
-    match rget(url.as_ref()) {
+pub fn _fetch<P: AsRef<str>>(url: P) -> anyhow::Result<Response> {
+    match get(url.as_ref()) {
         Ok(e) => Ok(e),
-        Err(e) => Err(Box::new(e)),
+        Err(e) => Err(anyhow::anyhow!(e))
+    }
+}
+
+pub fn _fetch2() {}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn fetch_test() {
+        let body = _fetch("https://nekos.best/api/v2/neko");
+        let body = body.unwrap();
+        let resp = body.text().unwrap();
+        println!("{resp}");
     }
 }
