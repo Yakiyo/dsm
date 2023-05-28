53c227d769b7012d340833f4d35e2abe2003d8fe
Yakiyo <yakiyo1206@gmail.com>
2023-05-28 14:39:05 +0000

    chore: cargo fmt

--- src/commands/ls.rs
+++ src/commands/ls.rs
@@ -7,8 +7,12 @@ pub struct Ls {}
 
 impl super::Command for Ls {
     fn run(self, config: DsmConfig) -> anyhow::Result<()> {
-        config.base_dir.ensure_dirs().context("Failed to ensure base dirs")?;
-        let dir_entries = fs::read_dir(&config.base_dir.installation_dir).context("Failed to read installation dir")?;
+        config
+            .base_dir
+            .ensure_dirs()
+            .context("Failed to ensure base dirs")?;
+        let dir_entries = fs::read_dir(&config.base_dir.installation_dir)
+            .context("Failed to read installation dir")?;
         for dir in dir_entries {
             let dir = dir.context("Error when reading dir")?;
             match dir.file_name().to_str() {
