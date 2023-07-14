87ebe5d8fe0ce80a137ee8ba5dfcb18bacd894c3
Yakiyo <yakiyo1206@gmail.com>
2023-07-14 15:37:54 +0000

    feat: possible fixes to windows path issue

--- src/commands/env.rs
+++ src/commands/env.rs
@@ -1,23 +1,36 @@
+use anyhow::Context;
 use std::collections::HashMap;
 
-use crate::shell::{Shell, AVAILABLE_SHELLS};
+use crate::shell::{fix_path, Shell, AVAILABLE_SHELLS};
 
 #[derive(clap::Args, Debug, Default)]
 pub struct Env {
     /// The shell syntax to use
     #[clap(possible_values = AVAILABLE_SHELLS)]
-    shell: Shell,
+    shell: Option<Shell>,
+
+    /// Print as json
+    #[clap(long, conflicts_with = "shell")]
+    json: bool,
 }
 
 impl super::Command for Env {
     fn run(self, config: crate::cli::DsmConfig) -> anyhow::Result<()> {
-        println!("{}", self.shell.path(&config.base_dir.bin)?);
         let env_vars = HashMap::from([
             ("DSM_ARCH", config.arch.to_string()),
             ("DSM_DIR", format!("{}", config.base_dir.root.display())),
-            ("DSM_COLORS", config.disable_colors.to_string()),
+            ("DSM_NO_COLORS", config.disable_colors.to_string()),
         ]);
-        self.shell.env_vars(&env_vars);
+
+        if self.json {
+            println!("{}", serde_json::to_string_pretty(&env_vars).unwrap());
+            return Ok(());
+        }
+
+        let shell = self.shell.with_context(|| "Missing argument for <SHELL>")?;
+        println!("{}", shell.path(&config.base_dir.bin)?);
+        let out = shell.env_vars(&env_vars);
+        println!("{}", fix_path(out.as_str()));
         Ok(())
     }
 }
