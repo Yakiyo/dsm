785ff90e49b0761cca9b52dc60fe1d5fe778ac1a
Yakiyo <yakiyo1206@gmail.com>
2023-08-04 03:22:30 +0000

    fix: current command

--- src/commands/current.rs
+++ src/commands/current.rs
@@ -5,7 +5,7 @@ pub struct Current;
 
 impl super::Command for Current {
     fn run(self, config: crate::config::Config) -> anyhow::Result<()> {
-        let res = config.base_dir.current_version()?;
+        let res = config.current_version()?;
         if let Some(version) = res {
             println!("{}", Paint::green(version.to_str()));
         } else {
