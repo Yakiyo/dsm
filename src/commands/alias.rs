c2ffee9d24bcdd8945c573f976888b6bd8c96592
Yakiyo <yakiyo1206@gmail.com>
2023-08-04 03:18:52 +0000

    fix: use pathbufs

--- src/commands/alias.rs
+++ src/commands/alias.rs
@@ -11,11 +11,16 @@ pub struct Alias {
 
 impl super::Command for Alias {
     fn run(self, config: Config) -> anyhow::Result<()> {
-        let alias_dir = &config.base_dir.aliases.join(&self.name);
+        let alias_dir = &config.aliases_dir().join(&self.name);
         if alias_dir.exists() {
             log::warn!("Alias with that name already exists. Overwriting it.");
         }
-        alias::create_alias(&config.base_dir, &self.version, &self.name)?;
+        alias::create_alias(
+            config.aliases_dir(),
+            config.installation_dir(),
+            &self.version,
+            &self.name,
+        )?;
         println!(
             "Created alias for {} with name {}",
             Paint::blue(format!("v{}", self.version)),
