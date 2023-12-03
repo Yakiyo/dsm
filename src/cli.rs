d19647fc58719df1c04c4d33d223d33951c8458c
Yakiyo <yakiyo1206@gmail.com>
2023-12-03 14:58:44 +0000

    feat: basic cli setup

---
+++ src/cli.rs
@@ -0,0 +1,41 @@
+use clap::{ArgAction, Parser, Subcommand};
+use log::LevelFilter;
+use std::path::PathBuf;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about)]
+#[command(disable_version_flag = true)] // disable default version flag since it uses `-V` for shorthand
+pub struct Cli {
+    /// Disable colors in output texts. Same as setting `NO_COLOR` to true
+    #[arg(long = "no-color", env = "NO_COLOR", global = true, hide_env(true))]
+    pub no_color: bool,
+
+    /// Path to config file. Defaults to `~/.config/dsm.toml`
+    #[arg(long, env = "DSM_CONFIG", global = true, hide_env(true))]
+    pub config: Option<PathBuf>,
+
+    /// Set log verbosity
+    #[arg(
+        long = "log-level", 
+        default_value = "error",
+        env = "DSM_LOG",
+        value_parser = ["off", "error", "warn", "info", "debug", "trace"],
+        hide_env(true),
+        hide_default_value(true),
+        hide_possible_values(true),
+    )]
+    pub log_level: LevelFilter,
+
+    /// Print version
+    #[arg(long, short, action = ArgAction::Version)]
+    pub version: bool,
+
+    #[command(subcommand)]
+    pub subcommand: Commands,
+}
+
+#[derive(Subcommand, Debug)]
+pub enum Commands {
+    Install,
+    Uninstall,
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use atty::Stream;
+use clap::Parser;
+use env_logger as logger;
+use yansi::Paint;
+
+mod cli;
+
 fn main() {
+    use cli::Cli;
+    let args = Cli::parse();
+
+    // configure log level
+    logger::builder()
+        .format_timestamp(None)
+        .format_module_path(false)
+        .filter_level(args.log_level)
+        .init();
+
+    // configure colors in output
+    if args.no_color || !Paint::enable_windows_ascii() || atty::isnt(Stream::Stdout) {
+        log::info!("Disable colors in output");
+        Paint::disable();
+    }
+
     println!("Hello World");
 }
